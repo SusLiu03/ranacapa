4feadf996f328d76deb7370cdf963ce4ecc5728f
SusLiu03 <susannl5@uci.edu>
2023-11-20 18:16:18 -0800

    making the port flexible

--- R/runRanacapaApp.R
+++ R/runRanacapaApp.R
@@ -1,11 +1,13 @@
 #' Run the shiny app!
 #' @export
 runRanacapaApp <- function() {
+  port <- Sys.getenv("RANACAPA_PORT")
+  port <- as.numeric(port)
   appDir <- system.file("explore-anacapa-output", package = "ranacapa")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `ranacapa`.", call. = FALSE)
   }
 
-  shiny::runApp(appDir, port = 6407, display.mode = "normal")
+  shiny::runApp(appDir, port = port, display.mode = "normal")
 }
 
