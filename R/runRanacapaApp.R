32710007a31fab6777e430997c78c8e7758c311c
SusLiu03 <susannl5@uci.edu>
2023-12-26 19:31:27 -0800

    retrying

--- R/runRanacapaApp.R
+++ R/runRanacapaApp.R
@@ -1,21 +1,21 @@
 # ' Run the shiny app!
 # ' @export
-runRanacapaApp <- function() {
-  port <- Sys.getenv("RANACAPA_PORT")
-  port <- as.numeric(port)
-  appDir <- system.file("explore-anacapa-output", package = "ranacapa")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `ranacapa`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, port = port, display.mode = "normal")
-}
-
 # runRanacapaApp <- function() {
+#   port <- Sys.getenv("RANACAPA_PORT")
+#   port <- as.numeric(port)
 #   appDir <- system.file("explore-anacapa-output", package = "ranacapa")
 #   if (appDir == "") {
 #     stop("Could not find example directory. Try re-installing `ranacapa`.", call. = FALSE)
 #   }
 
-#   shiny::runApp(appDir, display.mode = "normal")
+#   shiny::runApp(appDir, port = port, display.mode = "normal")
 # }
+
+runRanacapaApp <- function() {
+  appDir <- system.file("explore-anacapa-output", package = "ranacapa")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `ranacapa`.", call. = FALSE)
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+}
